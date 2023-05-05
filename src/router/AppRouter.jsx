2eb7140215128d4827862b4bc7d38d8a72f2f2cd
Mauro Ybañez <maurogabriely@gmail.com>
2023-05-04 23:15:54 -0300

    primero comit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth/pages/LoginPage'
+import { RegisterPage } from '../auth/pages/RegisterPage'
+
+
+
+export const AppRouter = () => {
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+    </Routes>
+
+  </BrowserRouter>
+
+  )
+}
